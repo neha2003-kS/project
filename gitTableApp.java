f3411ea55538c7017924d75d88bf9c89bfcd582d
Neha <nehakshr003@gmail.com>
2025-02-23 13:23:27 +0530

    10 table

---
+++ gitTableApp.java
@@ -0,0 +1,13 @@
+public class gitTable
+{
+	public static void main(String x[])
+	{
+		System.out.println("Enter number:");
+		int num=s.nextInt();
+
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(num*i);
+		}
+	}
+}
