5f035a005d53de1a77cd9db35f4e6b0d197ee01d
neha2003-kS <nehakshr003@gmail.com>
2025-02-23 13:51:32 +0530

    Add files via upload

---
+++ gitSquareApp.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class gitSquareApp
+{
+	public static void main(String x[])
+	{
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Enter number:");
+		int num=s.nextInt();
+
+		int sqr = num * num;
+		System.out.println("Square of " +num+ " : " + sqr);
+	}
+}
+	
